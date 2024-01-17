635173b419f753ddf53c7040e61996719a6495a7
Enrique Lomba <enrique.lomba@gmail.com>
2024-01-17 11:21:32 +0100

    retain dimsh

--- gpucodes.f90
+++ gpucodes.f90
@@ -171,7 +171,7 @@ end function fij
     real, intent(IN) :: sidel(3)
     real, dimension(dim,Nmol), intent(IN) :: r
     integer, intent(INOUT) :: histomix(hdim,nit)
-    integer, shared :: histomix_s(dimsh,nitmax)
+    integer, shared :: histomix_s(lsmax,nit)
     histomix_s(:,:) = 0
     i = (blockidx%x-1) * blockdim%x + threadidx%x
     if (i<=Nmol-1) then
